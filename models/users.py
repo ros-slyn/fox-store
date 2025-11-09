e3fc04b96359e2ce4149511dbf8d39de11867011
ros-slyn <chearosslyn755@gmail.com>
2025-11-09 23:50:18 +0700

    1

--- models/users.py
+++ models/users.py
@@ -7,7 +7,7 @@ class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128))
     email = db.Column(db.String(128))
-    password = db.Column(db.String(128))
+    password = db.Column(db.String(500))
     gender = db.Column(db.String(128), default='male')
     profile = db.Column(db.String(128), nullable=True)
     is_admin = db.Column(db.Boolean, default=False)  # Add this field
